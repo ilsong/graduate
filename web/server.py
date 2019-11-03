c02f9e003c5122a9d2b902dd5317ddf740798364
gun0317 <39793048+gun0317@users.noreply.github.com>
2019-11-03 11:59:05 +0900

    update

--- web/server.py
+++ web/server.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from graph_func import get_subgraph
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("/index.html")
+
+@app.route('/get_graph', methods = ['GET'])
+def get_graph():
+    if request.method == 'GET':
+        search_str = requst.form.get('input_str')
+        search_str = search_str.split('/w/')[1].replace("%20"," ")
+        if '?from=' in search_str:
+            search_str = search_str.split('?from=')[0]
+        result = get_subgraph(search_str)
+        return result
 
 if __name__ == '__main__':
    app.run(debug = True)
