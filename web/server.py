ef6593a7a69ced42f22f6b11a7878c0eabbf6ca1
gun0317 <39793048+gun0317@users.noreply.github.com>
2019-11-03 17:20:51 +0900

    web update

--- web/server.py
+++ web/server.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from graph_func import get_subgraph
+import json
+import pickle
+import networkx as nx
 app = Flask(__name__)
+
+# nx_digraph = nx.read_gpickle(path="data/graph.bin")
+# pagerank = pickle.load(open("data/pagerank.bin", "rb"))
 
 @app.route('/')
 def home():
     return render_template("/index.html")
 
-@app.route('/get_graph', methods = ['GET'])
+@app.route('/query', methods = ['POST'])
 def get_graph():
-    if request.method == 'GET':
-        search_str = requst.form.get('input_str')
-        search_str = search_str.split('/w/')[1].replace("%20"," ")
-        if '?from=' in search_str:
-            search_str = search_str.split('?from=')[0]
-        result = get_subgraph(search_str)
-        return result
+    search_str = request.form.get('query_str')
+    # result = get_subgraph(nx_digraph, search_str, pagerank, 6)
+
+    # sample result:
+    result = nx.path_graph(8)
+
+    return json.dumps(result)
 
 if __name__ == '__main__':
    app.run(debug = True)
